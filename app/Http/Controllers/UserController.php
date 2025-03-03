a0cbea445d1d1d99848a0cc13f1b950d763faef2
uhamhz <ammarhafizh2404@gmail.com>
2025-03-03 09:23:52 +0700

    update : praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,11 +11,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+       $data = [
+           'level_id' => 2,
+           'username' => 'manager_tiga',
+           'nama' => 'Manager Tiga',
+           'password' => Hash::make('12345')
+       ];
+       UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
