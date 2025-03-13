92d9cee1a2a31b98ec9eecac6d45fdadc74fd624
uhamhz <ammarhafizh2404@gmail.com>
2025-03-13 21:44:41 +0700

    update : praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -43,7 +44,7 @@ class UserController extends Controller
       if ($request->level_id) {
          $users = $users->where('level_id', $request->level_id);
       }
-      
+
       return DataTables::of($users)
          // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
          ->addIndexColumn()
@@ -195,5 +196,45 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
          // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
          return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
       }
+   }
+
+   public function create_ajax()
+   {
+      $level = LevelModel::select('level_id', 'level_nama')->get();
+
+      return view('user.create_ajax')
+         ->with('level', $level);
+   }
+
+   public function store_ajax(Request $request)
+   {
+      // cek apakah request berupa ajax
+      if ($request->ajax() || $request->wantsJson()) {
+         $rules = [
+            'level_id'  => 'required|integer',
+            'username'  => 'required|string|min:3|unique:m_user,username',
+            'nama'      => 'required|string|max:100',
+            'password'  => 'required|min:6'
+         ];
+
+         // use Illuminate\Support\Facades\Validator;
+         $validator = Validator::make($request->all(), $rules);
+
+         if ($validator->fails()) {
+            return response()->json([
+               'status'  => false, // response status, false: error/gagal, true: berhasil
+               'message' => 'Validasi Gagal',
+               'msgField' => $validator->errors(), // pesan error validasi
+            ]);
+         }
+
+         UserModel::create($request->all());
+         return response()->json([
+            'status'  => true,
+            'message' => 'Data user berhasil disimpan'
+         ]);
+      }
+
+      redirect('/');
    }
 }
