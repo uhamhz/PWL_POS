b2b5a915f1a4be57d983c1aabaeb48f3ae0ae584
uhamhz <ammarhafizh2404@gmail.com>
2025-04-06 21:27:05 +0700

    update : praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -123,5 +123,6 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [BarangController::class, 'destroy']);
         Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+        Route::get('/export_excel', [BarangController::class, 'export_excel']);// export excel
     });
 });
